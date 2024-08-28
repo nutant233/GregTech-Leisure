0664c5eb7f4b94074fda2264ea5f9a4b1196acb0
mul <1040179812@qq.com>
2024-08-28 20:11:32 +0800

    Initial commit

---
+++ .minecraft/kubejs/server_scripts/ae2.js
@@ -0,0 +1,341 @@
+//priority: 94
+ServerEvents.recipes((event) => {
+    const gtr = event.recipes.gtceu
+
+    event.remove({ id: "ae2:misc/deconstruction_certus_quartz_block" })
+    event.remove({ id: "ae2:network/blocks/controller" })
+    event.remove({ id: "ae2:network/crafting/patterns_blank" })
+    event.remove({ id: "ae2:network/parts/export_bus" })
+    event.remove({ id: "ae2:network/parts/import_bus" })
+    event.remove({ id: "ae2:network/wireless_part" })
+    event.remove({ id: "ae2:materials/annihilationcore" })
+    event.remove({ id: "ae2:materials/formationcore" })
+    event.remove({ id: "ae2:materials/advancedcard" })
+    event.remove({ id: "ae2:materials/basiccard" })
+    event.remove({ id: "ae2:network/crafting/cpu_crafting_unit" })
+    event.replaceInput({ id: "expatternprovider:fishbig" }, "minecraft:pufferfish", "gtceu:cosmic_ingot")
+    event.shapeless("infinitycells:infinity_cell", ["ae2:item_cell_housing", "kubejs:infinite_cell_component"])
+    event.shapeless("infinitycells:infinity_fluid_cell", ["ae2:fluid_cell_housing", "kubejs:infinite_cell_component"])
+
+    event.shaped("ae2:creative_energy_cell", [
+        "AAA",
+        "ADA",
+        "AAA"
+    ], {
+        A: "ae2:dense_energy_cell",
+        D: "gtceu:hv_field_generator"
+    })
+
+    event.shaped("ae2:crafting_unit", [
+        "PCP",
+        "KMK",
+        "PLP"
+    ], {
+        P: "gtceu:aluminium_plate",
+        C: "ae2:calculation_processor",
+        K: "ae2:fluix_glass_cable",
+        M: "#gtceu:circuits/mv",
+        L: "ae2:logic_processor",
+    })
+
+    event.shaped("2x ae2:advanced_card", [
+        "WP ",
+        "ACP",
+        "SP "
+    ], {
+        W: "#forge:fine_wires/red_alloy",
+        P: "#forge:plates/stainless_steel",
+        A: "ae2:engineering_processor",
+        C: "#gtceu:circuits/hv",
+        S: "#forge:fine_wires/silver",
+    })
+
+    event.shaped("2x ae2:basic_card", [
+        "WP ",
+        "ACP",
+        "SP "
+    ], {
+        W: "#forge:fine_wires/red_alloy",
+        P: "#forge:plates/aluminium",
+        A: "ae2:engineering_processor",
+        C: "#gtceu:circuits/mv",
+        S: "#forge:fine_wires/gold",
+    })
+
+    event.shaped("ae2:annihilation_core", [
+        "CPB"
+    ], {
+        C: "#gtceu:circuits/ulv",
+        P: "ae2:logic_processor",
+        B: "#forge:dusts/nether_quartz",
+    })
+
+    event.shaped("ae2:formation_core", [
+        "CPB"
+    ], {
+        C: "#gtceu:circuits/ulv",
+        P: "ae2:logic_processor",
+        B: "#forge:dusts/certus_quartz",
+    })
+
+    event.shaped("ae2:wireless_receiver", [
+        "CPC",
+        "RSR",
+        "   "
+    ], {
+        C: "#gtceu:circuits/ev",
+        P: "ae2:fluix_pearl",
+        R: "#forge:rods/titanium",
+        S: "gtceu:hv_sensor",
+    })
+
+    event.shaped("ae2:import_bus", [
+        "IPC"
+    ], {
+        I: "ae2:annihilation_core",
+        P: "gtceu:lv_robot_arm",
+        C: "ae2:fluix_glass_cable",
+    })
+
+    event.shaped("ae2:export_bus", ["IPC"], {
+        I: "ae2:formation_core",
+        P: "gtceu:lv_robot_arm",
+        C: "ae2:fluix_glass_cable",
+    })
+
+    gtr.forming_press("ae2:logic_processor")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:logic_processor_press")
+        .itemInputs("#forge:plates/gold", "ae2:silicon", "#forge:plates/redstone")
+        .itemOutputs("ae2:logic_processor")
+        .duration(200)
+        .EUt(30)
+
+    gtr.forming_press("ae2:logic_processor1")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:logic_processor_press")
+        .itemInputs("gtceu:gold_dust", "gtceu:silicon_dust", "minecraft:redstone")
+        .itemOutputs("ae2:logic_processor")
+        .duration(20)
+        .EUt(480)
+
+    gtr.forming_press("ae2:calculation_processor")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:calculation_processor_press")
+        .itemInputs("#forge:plates/certus_quartz", "ae2:silicon", "#forge:plates/redstone")
+        .itemOutputs("ae2:calculation_processor")
+        .duration(200)
+        .EUt(30)
+
+
+    gtr.forming_press("ae2:calculation_processor1")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:calculation_processor_press")
+        .itemInputs("gtceu:certus_quartz_dust", "gtceu:silicon_dust", "minecraft:redstone")
+        .itemOutputs("ae2:calculation_processor")
+        .duration(20)
+        .EUt(480)
+
+    gtr.forming_press("ae2:engineering_processor1")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:engineering_processor_press")
+        .itemInputs("#forge:plates/diamond", "ae2:silicon", "#forge:plates/redstone")
+        .itemOutputs("ae2:engineering_processor")
+        .duration(200)
+        .EUt(30)
+
+    gtr.forming_press("ae2:engineering_processor")
+        .notConsumable("ae2:silicon_press")
+        .notConsumable("ae2:engineering_processor_press")
+        .itemInputs("gtceu:diamond_dust", "gtceu:silicon_dust", "minecraft:redstone")
+        .itemOutputs("ae2:engineering_processor")
+        .duration(20)
+        .EUt(480)
+
+    gtr.mixer("ae2:fluix_dust")
+        .itemInputs("#forge:dusts/nether_quartz", "#forge:dusts/certus_quartz", "#forge:dusts/redstone")
+        .itemOutputs("3x ae2:fluix_dust")
+        .duration(200)
+        .EUt(16)
+
+    gtr.autoclave("ae2:fluix_crystal_1")
+        .itemInputs("#forge:dusts/fluix")
+        .inputFluids("water 250")
+        .itemOutputs("ae2:fluix_crystal")
+        .duration(1200)
+        .EUt(24)
+
+    gtr.autoclave("ae2:fluix_crystal_2")
+        .itemInputs("#forge:dusts/fluix")
+        .inputFluids("gtceu:distilled_water 50")
+        .itemOutputs("ae2:fluix_crystal")
+        .duration(600)
+        .EUt(24)
+
+    gtr.macerator("gtceu:silicon_dust")
+        .itemInputs("ae2:silicon")
+        .itemOutputs("gtceu:silicon_dust")
+        .EUt(16)
+        .duration(200)
+
+    gtr.macerator("ae2:fluix_dust")
+        .itemInputs("ae2:fluix_crystal")
+        .itemOutputs("ae2:fluix_dust")
+        .EUt(2)
+        .duration(240)
+
+    gtr.assembler("ae2:fluix_covered_cable")
+        .itemInputs("ae2:fluix_glass_cable")
+        .inputFluids("gtceu:rubber 4")
+        .itemOutputs("ae2:fluix_covered_cable")
+        .EUt(30)
+        .duration(20)
+
+    gtr.assembler("ae2:fluix_covered_cable1")
+        .itemInputs("ae2:fluix_glass_cable")
+        .inputFluids("gtceu:silicone_rubber 2")
+        .itemOutputs("ae2:fluix_covered_cable")
+        .EUt(30)
+        .duration(20)
+
+    gtr.assembler("ae2:fluix_covered_cable2")
+        .itemInputs("ae2:fluix_glass_cable")
+        .inputFluids("gtceu:styrene_butadiene_rubber 1")
+        .itemOutputs("ae2:fluix_covered_cable")
+        .EUt(30)
+        .duration(20)
+
+    gtr.assembler("gtceu:blank_pattern")
+        .itemInputs(
+            "3x #forge:plates/steel",
+            "2x #forge:plates/polyethylene",
+            "4x #forge:fine_wires/red_alloy",
+            "#gtceu:circuits/mv")
+        .itemOutputs("ae2:blank_pattern")
+        .inputFluids("gtceu:glass 144")
+        .duration(200)
+        .EUt(120)
+
+    gtr.assembler("gtceu:blank_pattern_good")
+        .itemInputs(
+            "3x #forge:plates/steel",
+            "2x #forge:plates/polyvinyl_chloride",
+            "4x #forge:fine_wires/red_alloy",
+            "#gtceu:circuits/mv")
+        .itemOutputs("4x ae2:blank_pattern")
+        .inputFluids("gtceu:glass 288")
+        .duration(200)
+        .EUt(120)
+
+    gtr.assembler("gtceu:blank_pattern_better")
+        .itemInputs(
+            "3x #forge:plates/steel",
+            "2x #forge:plates/polytetrafluoroethylene",
+            "4x #forge:fine_wires/red_alloy",
+            "#gtceu:circuits/mv")
+        .itemOutputs("16x ae2:blank_pattern")
+        .inputFluids("gtceu:glass 576")
+        .duration(200)
+        .EUt(480)
+
+    gtr.assembler("gtceu:blank_pattern_best")
+        .itemInputs(
+            "3x #forge:plates/steel",
+            "2x #forge:plates/polybenzimidazole",
+            "4x #forge:fine_wires/red_alloy",
+            "#gtceu:circuits/mv")
+        .itemOutputs("64x ae2:blank_pattern")
+        .inputFluids("gtceu:glass 1152")
+        .duration(200)
+        .EUt(480)
+
+    gtr.macerator("ae2:sky_dust")
+        .itemInputs("ae2:sky_stone_block")
+        .itemOutputs("ae2:sky_dust")
+        .EUt(2)
+        .duration(200)
+
+    gtr.chemical_bath("ae2:flawless_budding_quartz")
+        .itemInputs("ae2:flawed_budding_quartz")
+        .inputFluids("minecraft:water 1000")
+        .itemOutputs("ae2:flawless_budding_quartz")
+        .EUt(30)
+        .duration(400)
+
+    function getCellComponent(index) {
+        return index > 4 ? `gtceu:cell_component_${4 ** (index - 5)}m` : `ae2:cell_component_${4 ** index}k`
+    }
+
+    for (let index = 0; index < 5; index++) {
+        event.shapeless(`gtceu:${4 ** index}m_storage`, ["ae2:crafting_unit", getCellComponent(index + 5)])
+        event.shapeless(`gtceu:item_storage_cell_${4 ** index}m`, ["ae2:item_cell_housing", getCellComponent(index + 5)])
+        event.shapeless(`gtceu:fluid_storage_cell_${4 ** index}m`, ["ae2:fluid_cell_housing", getCellComponent(index + 5)])
+        event.shaped(`gtceu:item_storage_cell_${4 ** index}m`, [
+            "ABA",
+            "BDB",
+            "CCC"
+        ], {
+            A: "ae2:quartz_glass",
+            B: "minecraft:redstone",
+            C: "minecraft:iron_ingot",
+            D: getCellComponent(index + 5)
+        })
+        event.shaped(`gtceu:fluid_storage_cell_${4 ** index}m`, [
+            "ABA",
+            "BDB",
+            "CCC"
+        ], {
+            A: "ae2:quartz_glass",
+            B: "minecraft:redstone",
+            C: "minecraft:copper_ingot",
+            D: getCellComponent(index + 5)
+        })
+    }
+
+    for (let index = 1; index < 10; index++) {
+        gtr.assembler(getCellComponent(index))
+            .itemInputs(getCellComponent(index - 1), "2x #gtceu:circuits/" + tiers[index - 1][0], "ae2:logic_processor", index > 4 ? "ae2:engineering_processor" : "gtceu:redstone_plate", "ae2:calculation_processor", "gtceu:quartzite_plate")
+            .inputFluids("gtceu:soldering_alloy 72")
+            .itemOutputs(getCellComponent(index))
+            .EUt(GTValues.VA[index - 1] / 4)
+            .duration(200)
+    }
+
+    gtr.assembly_line("gtceu:256g_storage")
+        .itemInputs("ae2:crafting_unit", "16x gtceu:cell_component_256m", "gtceu:data_bank", "16x gtceu:data_orb", "4x #gtceu:circuits/uhv", "4x gtceu:double_red_steel_plate")
+        .inputFluids("gtceu:soldering_alloy 576", "gtceu:pcb_coolant 2000")
+        .itemOutputs("gtceu:256g_storage")
+        .EUt(GTValues.VA[GTValues.UV])
+        .duration(400)
+    ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack(Item.of("gtceu:256m_storage")).dataStack(Item.of("gtceu:data_stick")).EUt(GTValues.VA[GTValues.LuV]).duration(1200))
+
+    gtr.assembly_line("kubejs:infinite_cell_component")
+        .itemInputs("16x gtceu:256g_storage", "gtceu:data_bank", "16x gtceu:data_module", "64x ae2:creative_energy_cell", "64x #gtceu:circuits/uev", "64x gtceu:ruthenium_trinium_americium_neutronate_hex_wire", "4x gtceu:double_neutronium_plate")
+        .inputFluids("gtceu:mutated_living_solder 20000","gtceu:tairitsu 20000", "gtceu:pcb_coolant 100000")
+        .itemOutputs("kubejs:infinite_cell_component")
+        .EUt(GTValues.VA[GTValues.UHV])
+        .duration(2400)
+    ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack(Item.of("gtceu:cell_component_256m")).dataStack(Item.of("gtceu:data_orb")).EUt(GTValues.VA[GTValues.UV]).duration(2400))
+
+    for (let index = 1; index < 5; index++) {
+        gtr.assembler(`mae2:${4 ** index}x_crafting_accelerator`)
+            .itemInputs(index == 1 ? "ae2:crafting_accelerator" : `mae2:${4 ** (index - 1)}x_crafting_accelerator`, (2 ** index) + "x ae2:engineering_processor", getCellComponent(index))
+            .inputFluids("gtceu:soldering_alloy " + 72 * (2 ** index))
+            .itemOutputs(`mae2:${4 ** index}x_crafting_accelerator`)
+            .EUt(GTValues.VA[index - 1])
+            .duration(200)
+    }
+
+    for (let index = 0; index < 33; index++) {
+        event.shaped(Item.of("expatternprovider:infinity_cell", `{record:{"#c":"ae2:i",id:"gtceu:programmed_circuit",tag:{Configuration:${index}}}}`), [
+            "BAB",
+            "ADA",
+            "CCC"
+        ], {
+            A: Item.of("gtceu:programmed_circuit", `{Configuration:${index}}`).weakNBT(),
+            B: "ae2:quartz_glass",
+            C: "minecraft:diamond",
+            D: "ae2:cell_component_16k"
+        })
+    }
+})
---
+++ .minecraft/kubejs/server_scripts/minecraft.js
@@ -0,0 +1,177 @@
+//priority: 98
+ServerEvents.recipes((event) => {
+    const gtr = event.recipes.gtceu
+
+    event.remove({ id: "minecraft:netherite_ingot" })
+    event.remove({ id: "minecraft:ender_eye" })
+
+    event.shaped("minecraft:sculk_sensor", [
+        "ABA",
+        "CDC",
+        "ABA"
+    ], {
+        A: "minecraft:echo_shard",
+        B: "gtceu:echo_dust",
+        C: "minecraft:note_block",
+        D: "minecraft:comparator"
+    })
+
+    event.shaped("minecraft:sculk_shrieker", [
+        "ABA",
+        "BDB",
+        "ABA"
+    ], {
+        A: "kubejs:echobone",
+        B: 'gtceu:echo_rod',
+        D: "minecraft:sculk_sensor"
+    })
+
+    gtr.chemical_reactor("gtceu:ghast_tear")
+        .itemInputs("#forge:tiny_dusts/potassium", "#forge:tiny_dusts/lithium")
+        .inputFluids("gtceu:salt_water 1000")
+        .itemOutputs("minecraft:ghast_tear")
+        .duration(400)
+        .EUt(30)
+
+    gtr.assembler("gtceu:skeleton_skull")
+        .itemInputs("9x minecraft:bone")
+        .itemOutputs("skeleton_skull")
+        .duration(100)
+        .EUt(120)
+
+    gtr.extractor("gtceu:bones")
+        .itemInputs("#minecraft:dirt")
+        .chancedOutput("bone", 25, 0)
+        .duration(100)
+        .EUt(16)
+
+    gtr.compressor("gtceu:spongebob")
+        .itemInputs("2x #forge:foils/polycaprolactam")
+        .itemOutputs("sponge")
+        .duration(200)
+        .EUt(2)
+
+    gtr.chemical_reactor("gtceu:nether_star")
+        .itemInputs("#forge:dusts/diamond", "#forge:dusts/iridium")
+        .inputFluids("gtceu:rocket_fuel 1000", "gtceu:nether_air 8000")
+        .itemOutputs("gtceu:nether_star_dust")
+        .duration(200)
+        .EUt(7680);
+
+    gtr.assembler("minecraft:nether_star")
+        .itemInputs("4x minecraft:soul_sand", "3x minecraft:wither_skeleton_skull")
+        .itemOutputs("minecraft:nether_star")
+        .duration(100)
+        .EUt(120)
+
+    gtr.assembler("minecraft:reinforced_deepslate")
+        .itemInputs("minecraft:deepslate", "gtceu:echo_block", "4x kubejs:echobone")
+        .inputFluids("gtceu:echo 1440")
+        .itemOutputs("minecraft:reinforced_deepslate")
+        .duration(200)
+        .EUt(480)
+
+    gtr.chemical_bath("kubejs:echobone")
+        .itemInputs("minecraft:bone")
+        .inputFluids("gtceu:echo 144")
+        .itemOutputs("kubejs:echobone")
+        .EUt(480)
+        .duration(400)
+
+    gtr.chemical_bath("minecraft:black_candle")
+        .itemInputs("minecraft:string")
+        .inputFluids("gtceu:oil 100")
+        .itemOutputs("minecraft:black_candle")
+        .EUt(120)
+        .duration(200)
+
+    gtr.sps_crafting("minecraft:sculk_catalyst")
+        .itemInputs("minecraft:terracotta")
+        .inputFluids("gtceu:mana 1000", "gtceu:echo 100")
+        .itemOutputs("minecraft:sculk_catalyst")
+        .EUt(GTValues.VA[GTValues.UEV])
+        .duration(20)
+
+    gtr.compressor("minecraft:bedrock")
+        .itemInputs("4x gtceu:bedrock_dust")
+        .itemOutputs("minecraft:bedrock")
+        .EUt(GTValues.VA[GTValues.UIV])
+        .duration(4000)
+
+    gtr.mixer("minecraft:glowstone_dust")
+        .itemInputs("gtceu:gold_dust", "minecraft:redstone")
+        .itemOutputs("2x minecraft:glowstone_dust")
+        .circuit(1)
+        .EUt(120)
+        .duration(200)
+
+    gtr.lava_furnace("lava_furnace")
+        .itemInputs("#forge:cobblestone")
+        .outputFluids("minecraft:lava 1000")
+        .EUt(16)
+        .duration(200)
+
+    gtr.lava_furnace("lava_furnace1")
+        .itemInputs("#forge:stone")
+        .outputFluids("minecraft:lava 1000")
+        .EUt(16)
+        .duration(200)
+    gtr.chemical_bath("minecraft:prismarine")
+        .itemInputs("minecraft:cobblestone")
+        .inputFluids("gtceu:salt_water 100")
+        .itemOutputs("minecraft:prismarine")
+        .EUt(30)
+        .duration(200)
+
+    gtr.dragon_egg_copier("gtceu:dragon_egg_copier")
+        .itemInputs("minecraft:dragon_egg")
+        .inputFluids("gtceu:biohmediumsterilized 100")
+        .itemOutputs("minecraft:dragon_egg")
+        .chancedOutput("minecraft:dragon_egg", 2000, 1000)
+        .EUt(GTValues.VA[GTValues.ZPM])
+        .duration(200)
+        .cleanroom(CleanroomType.LAW_CLEANROOM)
+
+    gtr.fermenter("minecraft:poisonous_potato")
+        .itemInputs("minecraft:potato")
+        .itemOutputs("minecraft:poisonous_potato")
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(400)
+
+    gtr.fermenter("minecraft:rotten_flesh")
+        .itemInputs("gtceu:meat_dust")
+        .itemOutputs("minecraft:rotten_flesh")
+        .EUt(GTValues.VA[GTValues.LV])
+        .duration(400)
+
+    gtr.electric_blast_furnace("minecraft:netherite_ingot")
+        .itemInputs("minecraft:netherite_scrap", "ad_astra:calorite_ingot")
+        .itemOutputs("minecraft:netherite_ingot")
+        .inputFluids("gtceu:neon 100")
+        .EUt(1920)
+        .duration(800)
+        .blastFurnaceTemp(6470)
+
+    gtr.chemical_reactor("minecraft:dragon_egg")
+        .itemInputs("minecraft:egg", "64x kubejs:warped_ender_pearl")
+        .inputFluids("gtceu:antimatter 1000", "gtceu:ender_eye 10000", "gtceu:sterilized_growth_medium 10000")
+        .chancedOutput("minecraft:dragon_egg", 9000, 0)
+        .duration(2000)
+        .EUt(GTValues.VA[GTValues.UV])
+
+    gtr.chemical_bath("minecraft:crying_obsidian")
+        .itemInputs("minecraft:obsidian")
+        .inputFluids("gtceu:antimatter 10")
+        .itemOutputs("minecraft:crying_obsidian")
+        .EUt(GTValues.VA[GTValues.HV])
+        .duration(200)
+        .cleanroom(CleanroomType.CLEANROOM)
+
+    gtr.assembler("netherite_upgrade_smithing_template")
+        .circuit(2)
+        .notConsumable("minecraft:netherite_upgrade_smithing_template")
+        .itemInputs("7x gtceu:diamond_dust", "minecraft:netherrack")
+        .itemOutputs("minecraft:netherite_upgrade_smithing_template")
+        .EUt(480)
+        .duration(200)
+})
